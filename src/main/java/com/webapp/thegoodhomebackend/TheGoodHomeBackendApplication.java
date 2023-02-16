7c007351da7e71e941d136c5fffcbba221b43c1f
JulieChabanis <julie.chabanis07@gmail.com>
2023-02-16 16:41:33 +0100

    Init back-end dependency

---
+++ src/main/java/com/webapp/thegoodhomebackend/TheGoodHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.thegoodhomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGoodHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGoodHomeBackendApplication.class, args);
+    }
+
+}
