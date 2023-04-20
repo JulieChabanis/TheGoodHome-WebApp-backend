8916edb0cec9a231c11d330f03d9471a477290bb
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-04-20 18:20:19 +0200

    add security API wip 4

--- src/main/java/com/webapp/thegoodhomebackend/security/SecurityConfig.java
+++ src/main/java/com/webapp/thegoodhomebackend/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.webapp.thegoodhomebackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,15 @@ public class SecurityConfig {
       httpSecurity
               .csrf().disable()
               .authorizeHttpRequests()
-              .requestMatchers("/api/**").permitAll()
+              .requestMatchers("/api/agencies").permitAll()
+              .requestMatchers("/api/appartments").permitAll()
+              .requestMatchers("/api/pdf").permitAll()
+              .requestMatchers("/api/contracts").permitAll()
+              .requestMatchers("/api/solde_paiements").permitAll()
+              .requestMatchers("/api/rent-receipt").permitAll()
+              .requestMatchers("/api/tenants").permitAll()
+              .and()
+              .authorizeHttpRequests().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
               .anyRequest().authenticated()
               .and()
               .formLogin()
