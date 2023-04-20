bba897fc19ee4d058c72ee16e763f194b62f1115
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-04-20 18:49:01 +0200

    add security API wip 5

--- src/main/java/com/webapp/thegoodhomebackend/security/SecurityConfig.java
+++ src/main/java/com/webapp/thegoodhomebackend/security/SecurityConfig.java
@@ -11,20 +11,57 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String AGENCIES_API_URL = "/api/agencies";
+    private static final String APPARTMENTS_API_URL = "/api/appartments";
+    private static final String PDF_API_URL = "/api/pdf";
+    private static final String CONTRACTS_API_URL = "/api/contracts";
+
+  private static final String PAYMENTS_API_URL = "/api/solde_paiements";
+  private static final String RENT_RECEIPT_API_URL = "/api/rent-receipt";
+  private static final String TENANTS_API_URL = "/api/tenants";
+
+
   @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
       httpSecurity
               .csrf().disable()
               .authorizeHttpRequests()
-              .requestMatchers("/api/agencies").permitAll()
-              .requestMatchers("/api/appartments").permitAll()
-              .requestMatchers("/api/pdf").permitAll()
-              .requestMatchers("/api/contracts").permitAll()
-              .requestMatchers("/api/solde_paiements").permitAll()
-              .requestMatchers("/api/rent-receipt").permitAll()
-              .requestMatchers("/api/tenants").permitAll()
+              .requestMatchers(HttpMethod.GET,AGENCIES_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST,AGENCIES_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, AGENCIES_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, AGENCIES_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, APPARTMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, APPARTMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, APPARTMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE,APPARTMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, PDF_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, PDF_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, PDF_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, PDF_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, CONTRACTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, CONTRACTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, CONTRACTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, CONTRACTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, PAYMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, PAYMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, PAYMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, PAYMENTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, RENT_RECEIPT_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, RENT_RECEIPT_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, RENT_RECEIPT_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, RENT_RECEIPT_API_URL).permitAll()
+              .requestMatchers(HttpMethod.GET, TENANTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.POST, TENANTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.PUT, TENANTS_API_URL).permitAll()
+              .requestMatchers(HttpMethod.DELETE, TENANTS_API_URL).permitAll()
               .and()
-              .authorizeHttpRequests().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+              .authorizeHttpRequests().requestMatchers(HttpMethod.GET, "/**").permitAll()
+              .and()
+              .authorizeHttpRequests().requestMatchers(HttpMethod.POST, "/**").permitAll()
+              .and()
+              .authorizeHttpRequests().requestMatchers(HttpMethod.PUT, "/**").permitAll()
+              .and()
+              .authorizeHttpRequests().requestMatchers(HttpMethod.DELETE, "/**").permitAll()
               .anyRequest().authenticated()
               .and()
               .formLogin()
